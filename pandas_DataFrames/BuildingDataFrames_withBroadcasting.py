4d7e28adebd64d41e113ffac66c3a42d4724bb99
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-02-08 11:52:20 -0500

    Create BuildingDataFrames_withBroadcasting.py

---
+++ pandas_DataFrames/BuildingDataFrames_withBroadcasting.py
@@ -0,0 +1,15 @@
+# Importing libraries
+import pandas as pd
+import numpy as np
+
+# Make a string with the value 'PA': state
+state = 'PA'
+
+# Construct a dictionary: data
+data = {'state':state, 'city':cities}
+
+# Construct a DataFrame from dictionary data: df
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+print(df)
