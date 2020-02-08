f7fa9c18a7c200048f4af282b8643ac24d86bc39
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-02-08 16:00:00 -0500

    Update PandasScatterPlots.py

--- pandas_DataFrames/PandasScatterPlots.py
+++ pandas_DataFrames/PandasScatterPlots.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# In this exercise, you're going to plot fuel efficiency (miles-per-gallon) versus horse-power for 392 automobiles manufactured from 1970 to 1982 from the UCI Machine Learning Repository.
+# The size of each circle is provided as a NumPy array called sizes. This array contains the normalized 'weight' of each automobile in the dataset.
+# All necessary modules have been imported and the DataFrame is available in the workspace as df.
 # Generate a scatter plot
 df.plot(kind='scatter', x='hp', y='mpg', s=sizes)
 
