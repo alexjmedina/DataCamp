9fc8473f0455051b6f93f0befcf9a1a48ba7dc06
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-02-08 17:27:42 -0500

    Update Median_vs_Mean.py

--- pandas_DataFrames/Median_vs_Mean.py
+++ pandas_DataFrames/Median_vs_Mean.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
 # In many data sets, there can be large differences in the mean and median value due to the presence of outliers.
 # In this exercise, you'll investigate the mean, median, and max fare prices paid by passengers on the Titanic and generate a box plot of the fare prices. This data set was obtained from Vanderbilt University.
 # All necessary modules have been imported and the DataFrame is available in the workspace as df.
