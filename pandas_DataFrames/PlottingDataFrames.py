00737e8052360a897eabf5253c6932f20cdfa62d
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-02-08 15:10:41 -0500

    Update PlottingDataFrames.py

--- pandas_DataFrames/PlottingDataFrames.py
+++ pandas_DataFrames/PlottingDataFrames.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
 # We have pre-loaded three columns of data from a weather data set - temperature, dew point, and pressure - but the problem is that pressure has different units of measure. The pressure data, measured in Atmospheres, has a different vertical scaling than that of the other two data columns, which are both measured in degrees Fahrenheit.
 # Plot all columns (default)
 df.plot()
