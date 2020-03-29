255aa78041e200b9ac1a6a5f3be109c5a9364c6a
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-03-29 15:02:06 -0400

    Create Regular_Expressions.py

---
+++ NLP/Regular_Expressions.py
@@ -0,0 +1,15 @@
+import re
+my_string = "Let's write RegEx!  Won't that be fun?  I sure think so.  Can you find 4 sentences?  Or perhaps, all 19 words?"
+
+# Write a pattern to match sentence endings: sentence_endings
+sentence_endings = r"[___]"
+
+# Split my_string on sentence endings and print the result
+print(re.____(____, ____))
+
+# Find all capitalized words in my_string and print the result
+capitalized_words = r"[___]\w+"
+print(re.____(____, ____))
+
+# Split my_string on spaces and print the result
+spaces = r"___"
