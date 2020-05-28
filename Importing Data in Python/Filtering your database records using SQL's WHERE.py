77ef9f13510bea4d39d23804807f202281bc3e5e
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-05-27 21:23:58 -0400

    Create Filtering your database records using SQL's WHERE.py

---
+++ Importing Data in Python/Filtering your database records using SQL's WHERE.py
@@ -0,0 +1,13 @@
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute("SELECT * FROM Employee WHERE EmployeeId >= 6")
+    df = pd.DataFrame(rs.fetchmany())
+    df.columns = rs.keys()
+
+# Print the head of the DataFrame df
+print(df.head())
