66ae4ef02f58478554c81ab22167bb39f9bacb9a
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-05-27 22:43:34 -0400

    Create Ordering your SQL records with ORDER BY.py

---
+++ Importing Data in Python/Ordering your SQL records with ORDER BY.py
@@ -0,0 +1,17 @@
+
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute("SELECT * FROM Employee ORDER BY BirthDate")
+    df = pd.DataFrame(rs.fetchall())
+
+    # Set the DataFrame's column names
+    df.columns = rs.keys()
+
+
+# Print head of DataFrame
+print(df.head())
