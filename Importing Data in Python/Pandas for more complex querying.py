2f450123676ee6786480e06325931411f5c87899
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-05-27 22:59:17 -0400

    Create Pandas for more complex querying.py

---
+++ Importing Data in Python/Pandas for more complex querying.py
@@ -0,0 +1,12 @@
+# Import packages
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create engine: engine
+
+
+# Execute query and store records in DataFrame: df
+
+
+# Print head of DataFrame
+print(df.head())
