74becb14bef3b4e2c1e8a2a7f255edee14c3538d
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-06-04 23:24:14 -0400

    Update INNER JOIN.py

--- Importing Data in Python/INNER JOIN.py
+++ Importing Data in Python/INNER JOIN.py
@@ -8,9 +8,9 @@ engine = create_engine('sqlite:///Chinook.sqlite')
 # Open engine in context manager
 # Perform query and save results to DataFrame: df
 with engine.connect() as con:
-    ____
-    ____
-    ____
-
+    rs = con.execute("SELECT Title, Name FROM Album INNER JOIN Artist on Album.ArtistID = Artist.ArtistID")
+    df = pd.DataFrame(rs.fetchall())
+    df.columns = rs.keys()
+    
 # Print head of DataFrame df
 print(df.head())
