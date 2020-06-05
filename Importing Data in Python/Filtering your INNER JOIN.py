ede0bddb4729e398df33dbe8871371bfc6b1704f
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-06-05 09:54:51 -0400

    Update Filtering your INNER JOIN.py

--- Importing Data in Python/Filtering your INNER JOIN.py
+++ Importing Data in Python/Filtering your INNER JOIN.py
@@ -7,8 +7,7 @@ engine = create_engine('sqlite:///Chinook.sqlite')
 
 # Open engine in context manager
 # Perform query and save results to DataFrame: df
-with engine.connect() as con:
-    df = pd.read_sql_query("SELECT * FROM Employee WHERE EmployeeId >= 6 ORDER BY BirthDate", engine)
-    
+df = pd.read_sql_query("SELECT * FROM PlaylistTrack INNER JOIN Track PlaylistTrack.TrackId = Track.TrackId WHERE Milliseconds < 250000", engine)
+
 # Print head of DataFrame df
 print(df.head())
