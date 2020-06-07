936c5978c25ff3da20684017d93d0dec2c3bb816
Alexander Medina <alexjmedina@users.noreply.github.com>
2020-06-06 20:22:35 -0400

    Update Web Scraping part 3.py

--- Importing Data in Python/Web Scraping part 3.py
+++ Importing Data in Python/Web Scraping part 3.py
@@ -5,9 +5,10 @@ import pandas as pd
 url = 'http://s3.amazonaws.com/assets.datacamp.com/course/importing_data_into_r/latitude.xls'
 
 # Read in all sheets of Excel file: xls
-xls = pd.read_excel(url)
+xls = pd.read_excel(url, sheet_name=None)
 
 # Print the sheetnames to the shell
-
+print(xls.keys())
 
 # Print the head of the first sheet (using its name, NOT its index)
+print(xls['1700'].head())
